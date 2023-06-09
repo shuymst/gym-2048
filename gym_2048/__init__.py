26b07abd8c9ec7a969a5ea27de9cbd5e16b13af1
Shuhei Yamashita <yamashita-shuhei@g.ecc.u-tokyo.ac.jp>
2023-06-09 15:42:40 +0900

    update

--- gym_2048/__init__.py
+++ gym_2048/__init__.py
@@ -1,6 +1,6 @@
 from gymnasium.envs.registration import register
 
 register(
-    id="TwentyFortyeight-v0",
-    entry_point="gym_2048.envs:TwentyFortyeight",
+    id="TwentyFortyEight-v0",
+    entry_point="gym_2048.envs:TwentyFortyEight",
 )
